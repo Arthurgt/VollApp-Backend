4fca628ffc9ee9c322e38d34077f6ecb0cb803be
Arthurgt <arturbabkiewicz@gmail.com>
2021-05-20 12:26:51 +0200

    Added complete backend

---
+++ BackEnd/src/main/java/com/babkiewicz/artur/BackEnd/service/UserService.java
@@ -0,0 +1,16 @@
+package com.babkiewicz.artur.BackEnd.service;
+
+import java.util.List;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.babkiewicz.artur.BackEnd.model.User;
+
+public interface UserService {
+
+	User save(User user);
+	User update(User user);
+	List<User> findAll();
+	User getUserByEmail(String email);
+	User getUser(Long id);
+}
